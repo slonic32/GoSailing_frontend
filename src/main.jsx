18b00bbea4c1d4a0af82c60d010f5048eaee054c
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2024-11-18 00:32:38 +0200

    homework 6 fix

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/~ahordiyevs/gosailing5/">
+        <BrowserRouter basename="/~ahordiyevs/gosailing6/">
           <App />
         </BrowserRouter>
       </PersistGate>
